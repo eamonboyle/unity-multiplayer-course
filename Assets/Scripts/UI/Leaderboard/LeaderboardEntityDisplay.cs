82d1481bd9e21547826d9f92948ca71810d93188
Eamon Boyle <blaowskate@hotmail.com>
2024-04-27 21:12:55 +0100

    Finish leaderboard, updating & sorting correctly

--- Assets/Scripts/UI/Leaderboard/LeaderboardEntityDisplay.cs
+++ Assets/Scripts/UI/Leaderboard/LeaderboardEntityDisplay.cs
@@ -1,18 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
+using Unity.Collections;
+using Unity.Netcode;
 using UnityEngine;
 
 public class LeaderboardEntityDisplay : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    [SerializeField] private TMP_Text displayText;
+    [SerializeField] private Color myColor;
 
+    private FixedString32Bytes playerName;
+    public ulong ClientId { get; private set; }
+    public int Coins { get; private set; }
+
+    public void Initialize(ulong clientId, FixedString32Bytes playerName, int coins)
+    {
+        this.playerName = playerName;
+        ClientId = clientId;
+
+        if (clientId == NetworkManager.Singleton.LocalClientId)
+        {
+            displayText.color = myColor;
+        }
+
+        UpdateCoins(coins);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateCoins(int coins)
     {
+        Coins = coins;
+        UpdateText();
+    }
 
+    public void UpdateText()
+    {
+        displayText.text = $"{transform.GetSiblingIndex() + 1}. {playerName} ({Coins})";
     }
 }
